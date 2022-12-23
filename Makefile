43b17d7757c9dcf3f081869ad0192b3e8b2d567c
databunks <sahaq2001@gmail.com>
2022-12-23 14:34:23 +0000

    MongoCXX setup And Makefile Updates

--- Makefile
+++ Makefile
@@ -1,12 +1,29 @@
 CXX = g++
-CXXFLAGS = -Wall
+CXXFLAGS = -Wall -std=c++17
 SERVERPATH = server/
 
-${SERVERPATH}lib/%.o: ${SERVERPATH}src/%.cpp
-	$(CXX) -c $< -o $@ ${CXXFLAGS} -I ${SERVERPATH}include/ 
 
-server: ${SERVERPATH}lib/Server.o
-	$(CXX) ${SERVERPATH}lib/Server.o -o ${SERVERPATH}bin/serverOutput
+
+CPP_BSONCXXX_PATH = /usr/local/include/bsoncxx/v_noabi
+CPP_MONGOCXX_PATH = /usr/local/include/mongocxx/v_noabi/
+
+CPP_LOCAL_LIB_PATH = /usr/local/lib
+
+${SERVERPATH}lib/%.o: ${SERVERPATH}src/%.cpp
+	$(CXX) -c  $< -o $@ ${CXXFLAGS} -I${CPP_MONGOCXX_PATH}  -I${CPP_BSONCXXX_PATH}  -L${CPP_LOCAL_LIB_PATH} -lmongocxx -lbsoncxx -I${SERVERPATH}include/  
+	
+
+server: ${SERVERPATH}lib/server.o
+	$(CXX) ${SERVERPATH}lib/server.o -o ${SERVERPATH}bin/serverOutput 
+
+registration: ${SERVERPATH}lib/registration.o
+	$(CXX) ${SERVERPATH}lib/registration.o -I${CPP_MONGOCXX_PATH}  -I${CPP_BSONCXXX_PATH}  -L${CPP_LOCAL_LIB_PATH} -lmongocxx -lbsoncxx -o ${SERVERPATH}bin/registrationOutput  
+
+include auth/*
+
+registrationTest: ${SERVERPATH}bin/registrationOutput
+	${SERVERPATH}bin/./registrationOutput $(username) $(password) $(cluster)
+
 
 clean:
 	rm -rf ${SERVERPATH}lib/*.o ${SERVERPATH}bin/*
--- server/src/registration.cpp
+++ server/src/registration.cpp
@@ -1,2 +1,52 @@
+#include <cstdint>
 #include <iostream>
+#include <vector>
+#include <string>
 
+#include <bsoncxx/json.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/stdx.hpp>
+#include <mongocxx/uri.hpp>
+#include <mongocxx/instance.hpp>
+#include <bsoncxx/builder/stream/helpers.hpp>
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/builder/stream/array.hpp>
+
+using bsoncxx::builder::stream::close_array;
+using bsoncxx::builder::stream::close_document;
+using bsoncxx::builder::stream::document;
+using bsoncxx::builder::stream::finalize;
+using bsoncxx::builder::stream::open_array;
+using bsoncxx::builder::stream::open_document;
+
+
+int main(int argc, char *argv[])
+{
+    // MongoCXX setup
+
+    mongocxx::instance instance{};
+
+    int requiredNumberOfArguments = 4;
+
+    if (argc < requiredNumberOfArguments)
+    {
+        std::cerr << "Invalid number of arguments (only " << argc - 1 << " passed in, need " << requiredNumberOfArguments << ")" << std::endl;
+        return 1;
+    }
+
+    std::string username {argv[1]};
+    std::string password {argv[2]}; 
+    std::string cluster {argv[3]};
+
+    std::string uriString = std::string("mongodb+srv://") + username + std::string(":") + password + std::string("@") + cluster + std::string(".xlmttbn.mongodb.net/?retryWrites=true&w=majority");
+    
+    mongocxx::client client{mongocxx::uri{uriString.c_str()}};
+
+    mongocxx::database db = client[cluster];
+
+    // TODO: Setup Asio to receive registration details
+
+    // TODO: Save And Verify Information To Save To Database
+
+    return 0;
+}
